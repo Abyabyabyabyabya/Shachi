65dfb8fc6c29562189963956c6a2ed3169236ce8
Abya <harutch.222@gmail.com>
2021-04-11 10:41:37 +0900

    ver 2.0 へ向けた eyeブランチを作成

--- shachi/main.cpp
+++ shachi/main.cpp
@@ -2,13 +2,13 @@
 #include <crtdbg.h>
 #endif
 #include <Windows.h>
-#include "shachi.hpp"
+
  
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 #ifdef _DEBUG
     //_CrtSetBreakAlloc();
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
-    return Shachi::run(hInstance);
+
 }
 // EOF
